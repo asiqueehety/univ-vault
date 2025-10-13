c495d30a21dee23838fdde74d84f43382f92359d
asiqueehety <asique228@gmail.com>
2025-10-13 07:38:33 +0600

    letssee

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
                 email: email,
                 pw : pw,
             }),
-            credentials: 'include',
+            credentials: "include", // ✅ critical
         });
         const data = await res.json();
         if (res.ok) {
